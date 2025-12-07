f6c6d49c9cc9a9e0d4b75b19d609c452da5632ec
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-07 22:37:43 +0530

    Done: tailwind i, router i and basic redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './App.css'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-amber-200'>
+      <h1>Welcome to india</h1>
+    </div>
   )
 }
 
---
+++ src/redux/pasteSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+// import type { PayloadAction } from '@reduxjs/toolkit'
+
+
+const initialState = {
+  pastes:localStorage.getItem("pastes")
+  ? JSON.parse(localStorage.getItem("pastes"))
+  : [],
+}
+
+export const pasteSlice = createSlice({
+  name: 'paste',
+  initialState,
+  reducers: {
+    addToPastes: (state,action) => {
+      
+    },
+    updateToPaste: (state,action) => {
+      
+    },
+    resetAllPastes: (state, action) => {
+      
+    },
+    removeFromPastes: (state,action) => {
+
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { addToPastes, updateToPaste, resetAllPastes, removeFromPastes } = pasteSlice.actions
+
+export default counterSlice.reducer
